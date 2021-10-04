19405eaced4553a65f09c04768dad64250ed9773
Francisco <fmorosini@gmail.com>
2021-10-04 09:55:08 -0300

    arreglo de tamaños de letra e imagen responsive

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.png';
+import logo from './logo.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+        <img src={logo} className="App-logo" alt="logo" />    
+        <span className="App-mail" >lumencoopvisual@gmail.com</span>   
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
